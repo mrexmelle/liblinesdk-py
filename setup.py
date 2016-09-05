9ab39d5cab34d582f939980a6d3ec247e1929bc0
Matthew Tanudjaja <mrexmelle@gmail.com>
2016-09-05 11:59:48 +0700

    refactor: Change README.rst to README.md

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
