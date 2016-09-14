40eab93995191c49c29e8db2e0e6be666f16dcc3
Matthew Tanudjaja <mrexmelle@gmail.com>
2016-09-15 01:31:35 +0700

    fix: Fix typos

--- liblinesdk/api/events.py
+++ liblinesdk/api/events.py
@@ -17,17 +17,17 @@ def send_text_message(access_token, to, to_type, text):
     content={'contentType': ContentType.text, 'toType': to_type, 'text': text}
     send_message(access_token, to, content)
 
-def send_image_mesage(access_token, to, to_type, original_content_url, preview_image_url):
+def send_image_message(access_token, to, to_type, original_content_url, preview_image_url):
     content={'contentType': ContentType.image, 'toType': to_type,
         'originalContentUrl': original_content_url, 'previewImageUrl': preview_image_url}
     return send_message(access_token, to, content)
 
-def send_video_mesage(access_token, to, to_type, original_content_url, preview_image_url):
+def send_video_message(access_token, to, to_type, original_content_url, preview_image_url):
     content={'contentType': ContentType.video, 'toType': to_type,
         'originalContentUrl': original_content_url, 'previewImageUrl': preview_image_url}
     return send_message(access_token, to, content)
 
-def send_audio_mesage(access_token, to, to_type, original_content_url, audio_length):
+def send_audio_message(access_token, to, to_type, original_content_url, audio_length):
     metadata={'AUDLEN': audio_length}
     content={'contentType': ContentType.audio, 'toType': to_type,
         'originalContentUrl': original_content_url, 'contentMetadata': metadata}
